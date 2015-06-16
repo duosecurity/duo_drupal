ce56fd8f5b38d1dc9c22e8722937f24d72bbcdc3
Matt Schwager <matts@duosecurity.com>
2015-06-16 16:09:05 -0400

    Cleanup according to Drupal coding standards.

--- 6.x/duo.php
+++ 6.x/duo.php
@@ -1,116 +1,116 @@
 <?php
 
 /**
+ * @file
+ * Helper functions for the Duo module.
  *
- *  @file
- *    Helper functions for the Duo module.  Includes the Duo API
- *    to reduce the number of files.
+ * Includes the Duo API to reduce the number of files.
  */
 
-  require_once('duo_web.php');
+  require_once 'duo_web.php';
 
 /**
- *  Generates a random alpha numeric string.
+ * Generates a random alpha numeric string.
  */
-  function generate_random_string() {
-    return bin2hex(generate_random_bytes(20));
+function generate_random_string() {
+  return bin2hex(generate_random_bytes(20));
+}
+
+/**
+ * Generates random bytes.
+ *
+ * Tries to read from /dev/urandom to create a random number of bytes. Falls
+ * back on hashing microtime.
+ */
+function generate_random_bytes($count) {
+  $output = '';
+  if (is_readable('/dev/urandom') && ($fh = @fopen('/dev/urandom', 'rb'))) {
+    $output = fread($fh, $count);
+    fclose($fh);
   }
 
-/**
- *  Tries to read from /dev/urandom to create a random number of bytes.  Falls back on hashing microtime.
- */
-  function generate_random_bytes($count) {
+  $random_state = microtime();
+
+  if (strlen($output) < $count) {
     $output = '';
-    if (is_readable('/dev/urandom') && ($fh = @fopen('/dev/urandom', 'rb'))) {
-      $output = fread($fh, $count);
-      fclose($fh);
+    for ($i = 0; $i < $count; $i += 16) {
+      $random_state = md5(microtime() . $random_state);
+      $output .= pack('H*', md5($random_state));
     }
-
-    $random_state = microtime();
-
-    if (strlen($output) < $count) {
-      $output = '';
-      for ($i = 0; $i < $count; $i += 16) {
-        $random_state = md5(microtime() . $random_state);
-        $output .= pack('H*', md5($random_state));
-      }
-      $output = substr($output, 0, $count);
-    }
-    return $output;
+    $output = substr($output, 0, $count);
   }
+  return $output;
+}
 
 
 /**
- *  HTML Generation Functions
+ * HTML Generation Functions.
  *
- *  The following functions will all generate an HTML page to be displayed before loggin the user in.
+ * The following functions will all generate an HTML page to be displayed
+ * before logging the user in.
  *
- *  @see _duo_generate_page()
- *  @see _duo_generate_help()
+ * @see _duo_generate_page()
+ * @see _duo_generate_help()
  */
 
 
 /**
- *  Generate Duo Login page header.
- *
- *  @return HTML for the Duo page header.
+ * Generate Duo Login page header.
  */
-  function  _duo_generate_page($sig_req, $apihostname, $reset = false) {
-    ob_start();
+function _duo_generate_page($sig_req, $apihostname, $reset = FALSE) {
+  ob_start();
 
-    include('resources/duo_header.php');
+  include 'resources/duo_header.php';
 
-    echo "\n\t<script src=\"" . base_path() . drupal_get_path('module', 'duo') . "/duo.js\" ></script>";
-    echo "\n\t<script>Duo.init({'host':'" . $apihostname . "','post_action':'" .
+  echo "\n\t<script src=\"" . base_path() . drupal_get_path('module', 'duo') . "/duo.js\" ></script>";
+  echo "\n\t<script>Duo.init({'host':'" . $apihostname . "','post_action':'" .
     url("duo_login_process", array("absolute" => TRUE)) . "','sig_request':'" . $sig_req . "'});";
-    echo "\n\t</script>\n";
+  echo "\n\t</script>\n";
 
-    echo '<style> body {text-align: center;} iframe {width: 100%; min-width: 304px; max-width: 620px; height: 330px;}</style>';
-    echo '<iframe id="duo_iframe" frameborder="0"></iframe>';
-    echo '<form id="duo_form" method="POST">' . "\n\t\t";
-    echo '<input type="hidden" name="reset" value=' . $reset . ' />';
-    echo "\n\t</form>";
+  echo '<style> body {text-align: center;} iframe {width: 100%; min-width: 304px; max-width: 620px; height: 330px;}</style>';
+  echo '<iframe id="duo_iframe" frameborder="0"></iframe>';
+  echo '<form id="duo_form" method="POST">' . "\n\t\t";
+  echo '<input type="hidden" name="reset" value=' . $reset . ' />';
+  echo "\n\t</form>";
 
-    include('resources/duo_footer.php');
+  include 'resources/duo_footer.php';
 
-    $doc = ob_get_clean();
-    @ob_end_clean();
-    return $doc;
-  }
+  $doc = ob_get_clean();
+  @ob_end_clean();
+  return $doc;
+}
 
 /**
- *  Generates Help Text.
+ * Generates Help Text.
  *
- *  This is just to remove all HTML from the module itself.
- *
- *  @return Help HTML
+ * This is just to remove all HTML from the module itself.
  */
-  function _duo_generate_help() {
-    $help = '<h1>' . t("Duo Two-Factor Authentication") . '</h1>';
+function _duo_generate_help() {
+  $help = '<h1>' . t("Duo Two-Factor Authentication") . '</h1>';
 
-    $help .= "<p>" . t("For more complete documentation, view Duo's") . ' <a href="http://www.duosecurity.com/docs/drupal">' . t('documentation on the module') . "</a>.</p>";
+  $help .= "<p>" . t("For more complete documentation, view Duo's") . ' <a href="http://www.duosecurity.com/docs/drupal">' . t('documentation on the module') . "</a>.</p>";
 
-    $help .= "<p>" . t("If you need additional help, contact") . ' <a href="mailto:support@duosecurity.com">support@duosecurity.com</a>.</p>';
+  $help .= "<p>" . t("If you need additional help, contact") . ' <a href="mailto:support@duosecurity.com">support@duosecurity.com</a>.</p>';
 
-    $help .= '<h2>' . t("About") . '</h2>';
+  $help .= '<h2>' . t("About") . '</h2>';
 
-    $help .= '<p>' . t("This module adds a second step to Drupal's existing login function by implementing") . " Duo's " . t("two-factor authentication API.") . '</p>';
+  $help .= '<p>' . t("This module adds a second step to Drupal's existing login function by implementing") . " Duo's " . t("two-factor authentication API.") . '</p>';
 
-    $help .= '<p>' . t("Duo's two-factor authentication requires a user to use their phone to log in, in addition to their username and password. After entering a valid username and password, the user will be asked to enroll with Duo. After they register their phones with Duo, they will be asked to authenticate with their phones before being signed in. On following attempts to log in, they won't need to enroll again, they'll only be asked to authenticate.") .  '</p>';
+  $help .= '<p>' . t("Duo's two-factor authentication requires a user to use their phone to log in, in addition to their username and password. After entering a valid username and password, the user will be asked to enroll with Duo. After they register their phones with Duo, they will be asked to authenticate with their phones before being signed in. On following attempts to log in, they won't need to enroll again, they'll only be asked to authenticate.") . '</p>';
 
-    $help .= '<h2>' . t("Setting up") . '</h2>';
+  $help .= '<h2>' . t("Setting up") . '</h2>';
 
-    $help .= '<p>' . t("To set up this module, you need to sign up with an account at") . ' <a href="http://www.duosecurity.com" target="_blank">http://www.duosecurity.com</a>.</p>';
+  $help .= '<p>' . t("To set up this module, you need to sign up with an account at") . ' <a href="http://www.duosecurity.com" target="_blank">http://www.duosecurity.com</a>.</p>';
 
-    $help .= '<p>' . t("After signing up with Duo, you will need to create an integration. ") . t("Duo offers a") . ' <a href="http://www.duosecurity.com/docs">' . t("getting started guide") . "</a> " . t("that explains how to set up an integration.") .'</p>';
+  $help .= '<p>' . t("After signing up with Duo, you will need to create an integration. Duo offers a") . ' <a href="http://www.duosecurity.com/docs">' . t("getting started guide") . "</a> " . t("that explains how to set up an integration.") . '</p>';
 
-    $help .= '<p>' . t("Upon creating an integration, you will be provided with an integration key, secret key, and API hostname. You'll need this information to configure the module.  Copy and paste the values from the Duo administrative interface into the module configuration page.") . '</p>';
+  $help .= '<p>' . t("Upon creating an integration, you will be provided with an integration key, secret key, and API hostname. You'll need this information to configure the module.  Copy and paste the values from the Duo administrative interface into the module configuration page.") . '</p>';
 
-    $help .= '<p>' . t("If the Duo plugin is left unconfigured, users will continue to login as usual without two-factor authentication.") . '</p>';
+  $help .= '<p>' . t("If the Duo plugin is left unconfigured, users will continue to login as usual without two-factor authentication.") . '</p>';
 
-    $help .= '<h2>' . t("Customization") . '</h2>';
+  $help .= '<h2>' . t("Customization") . '</h2>';
 
-    $help .= '<p>' . t("You can customize the look of the login page by editing the files") . " duo_header.php, duo_footer.php " . t("and") . " custom.css " . t("in the module's") . " resources " . t("folder. The login form is rendered in between the header and footer files.");
+  $help .= '<p>' . t("You can customize the look of the login page by editing the files") . " duo_header.php, duo_footer.php " . t("and") . " custom.css " . t("in the module's") . " resources " . t("folder. The login form is rendered in between the header and footer files.");
 
-    return $help;
-  }
+  return $help;
+}
--- 6.x/duo_strings.php
+++ 6.x/duo_strings.php
@@ -1,53 +1,53 @@
 <?php
 /**
- * Titles
+ * @file
+ * Titles.
  */
-  // Config title strings
-  $duo_titles['config_form'] = t('Duo two-factor configuration');
-  $duo_titles['integration_key'] = t('Integration key');
-  $duo_titles['secret_key'] = t('Secret key');
-  $duo_titles['api_hostname'] = t('API hostname');
-  $duo_titles['form_preview'] = t('Form preview');
-  $duo_titles['preview_url'] = t('View the login/enrollment form.');
 
-  // Permission title string
-  $duo_titles['permission'] = t('Log in with Duo two-factor authentication');
+// Config title strings.
+$duo_titles['config_form'] = t('Duo two-factor configuration');
+$duo_titles['integration_key'] = t('Integration key');
+$duo_titles['secret_key'] = t('Secret key');
+$duo_titles['api_hostname'] = t('API hostname');
+$duo_titles['form_preview'] = t('Form preview');
+$duo_titles['preview_url'] = t('View the login/enrollment form.');
+
+// Permission title string.
+$duo_titles['permission'] = t('Log in with Duo two-factor authentication');
 
 
 /**
- * Descriptions
+ * Descriptions.
  */
 
-  // Config description strings
-  $duo_descriptions['config_form'] = t('Setup the Duo module with your integration settings.');
-  $duo_descriptions['integration_key'] = t('Integration key from the Duo administrative interface');
-  $duo_descriptions['secret_key'] = t('Secret key from the Duo administrative interface');
-  $duo_descriptions['api_hostname'] = t('API hostname from the Duo administrative interface');
-  $duo_descriptions['form_preview'] = t("Follow the link above to ensure that you have configured the module correctly.  
-  If you don't see an enrollment/login form, verify that you have entered the proper keys and hostname.  If the problem persists, 
-  disable the module and contact") . ' <a href="mailto:support@duosecurity.com">support@duosecurity.com</a>';
+// Config description strings.
+$duo_descriptions['config_form'] = t('Setup the Duo module with your integration settings.');
+$duo_descriptions['integration_key'] = t('Integration key from the Duo administrative interface');
+$duo_descriptions['secret_key'] = t('Secret key from the Duo administrative interface');
+$duo_descriptions['api_hostname'] = t('API hostname from the Duo administrative interface');
+$duo_descriptions['form_preview'] = t("Follow the link above to ensure that you have configured the module correctly.  If you don't see an enrollment/login form, verify that you have entered the proper keys and hostname.  If the problem persists, disable the module and contact") . ' <a href="mailto:support@duosecurity.com">support@duosecurity.com</a>';
 
-  // Permission description string
-  $duo_descriptions['permission'] = t('Require the selected roles to authenticate with two-factor authentication.');
+// Permission description string.
+$duo_descriptions['permission'] = t('Require the selected roles to authenticate with two-factor authentication.');
 
 
 /**
- *  Error Messages
+ * Error Messages.
  */
 
-  // Login errors
-  $duo_error['logged_in'] = t("You are already logged in.");
-  $duo_error['login_invalid'] = t("Invalid username or password.");
+// Login errors.
+$duo_error['logged_in'] = t("You are already logged in.");
+$duo_error['login_invalid'] = t("Invalid username or password.");
 
-  // Config errors
-  $duo_error['invalid_ikey'] = t("The integration key you have entered is the incorrect length.  Please check the value before trying again.");
-  $duo_error['invalid_skey'] = t("The secret key you have entered is the incorrect length.  Please check the value before trying again.");
-  $duo_error['short_api'] = t("The API hostname you have entered is too short.  Please check the value before trying again.");
-  $duo_error['bad_configuration'] = t("The module hasn't been configured properly.  Check your settings before trying again.");
+// Config errors.
+$duo_error['invalid_ikey'] = t("The integration key you have entered is the incorrect length.  Please check the value before trying again.");
+$duo_error['invalid_skey'] = t("The secret key you have entered is the incorrect length.  Please check the value before trying again.");
+$duo_error['short_api'] = t("The API hostname you have entered is too short.  Please check the value before trying again.");
+$duo_error['bad_configuration'] = t("The module hasn't been configured properly.  Check your settings before trying again.");
 
-  // Two-factor errors
-  $duo_error['tfa_not_recieved'] = t("The required information wasn't received.  Please try again.");
-  $duo_error['tfa_invalid'] = t("Secondary authentication credentials were invalid, or the user cannot be loaded.");
+// Two-factor errors.
+$duo_error['tfa_not_recieved'] = t("The required information wasn't received.  Please try again.");
+$duo_error['tfa_invalid'] = t("Secondary authentication credentials were invalid, or the user cannot be loaded.");
 
-  // PHP version error
-  $duo_error['php_invalid'] = t("You are running an unsupported") . " PHP " . t("installation.") . " PHP >= 5.1.2, PECL >= 1.1" . t("required.");
+// PHP version error.
+$duo_error['php_invalid'] = t("You are running an unsupported") . " PHP " . t("installation.") . " PHP >= 5.1.2, PECL >= 1.1" . t("required.");
